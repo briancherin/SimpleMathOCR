f796cbb5cc702244a2cd7ec3fc60ec9129140c92
Brian <brncherin@gmail.com>
2018-05-31 16:04:55 -0400

    Graphical view of contour points using matplotlib

--- contours2.py
+++ contours2.py
@@ -1,6 +1,7 @@
 import cv2
 import imutils
 import numpy as np
+from matplotlib import pyplot as plt
 
 im1 = cv2.imread("symb.jpg") #Import image as grayscale
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY);
@@ -22,7 +23,7 @@ im_copy = im1
 image, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 cv2.namedWindow('t')
-cv2.createTrackbar('precision', 't', 0, 100, nothing)
+#cv2.createTrackbar('precision', 't', 0, 100, nothing)
 cv2.createTrackbar('ind', 't', 0, len(contours)-1, nothing)
 
 
@@ -36,7 +37,7 @@ for c in sorted(contours, key=len):
 
 while True:
 	
-	precision = cv2.getTrackbarPos('precision', 't') / 100.0 #level of approximation precision
+	precision = 0.05 	#cv2.getTrackbarPos('precision', 't') / 100.0 #level of approximation precision
 	ind = cv2.getTrackbarPos('ind', 't') #index of contours to use (?)
 	
 	#Simplify the contours by approximating the polygonal curves of the contour
@@ -44,11 +45,40 @@ while True:
 	approx = cv2.approxPolyDP(contours[ind], epsilon, True)
 
 	cont = cv2.drawContours(im1.copy(), approx, -1, (0,255,0),3)
+	print("approximated contour:")
+	print(type(approx[0][0][1]))
 	
-
+	coordinates = approx
+	
+	x = []
+	y = []
+	
+	print("There are " + str(len(coordinates)) + " coordinates")
+	
+	for coord in coordinates:
+		x.append(coord[0][0])
+		y.append(-1*coord[0][1])
+	for n in range (0, len(x)):
+		print(str(x[n]) + ", " + str(y[n]))
+	
+	x = np.array(x)
+	y = np.array(y)
+	
+	#determine center coordinate:
+	xcenter = np.sum(x.copy()) / len(x)
+	ycenter = np.sum(y.copy()) / len(y)
+	
+	print("Center: " + str(xcenter) + ", " + str(ycenter))
+	
+	
+	
 	cv2.imshow("result", np.hstack([thresh, cont]))
 	
-	if cv2.waitKey(1) & 0xFF == ord("q"):
+	k = cv2.waitKey(1)
+	if k == ord("g"):
+		plt.scatter(x, y)
+		plt.show()
+	if k & 0xFF == ord("q"):
 		break;
 cv2.destroyAllWindows()
 
---
+++ mpltest.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+x = np.array([1, 2, 3])
+y = np.array([3, 6, 9])
+
+plt.scatter(x, y)
+plt.show()
